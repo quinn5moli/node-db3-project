1a7a5980eaa69336cfcd6196cc23cbaa055ec41c
quinn5moli <quinn.moli@gmail.com>
2021-11-16 20:35:57 -0800

    middleware finished

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -15,7 +15,7 @@ const checkSchemeId = async (req, res, next) => {
     .first()
 
     if(!checkId) {
-      next({ status: 404, message: `scheme with id ${req.params.scheme_id} not found `})
+      next({ status: 404, message: `scheme with scheme_id ${req.params.scheme_id} not found `})
     } else {
       next()
     }
@@ -33,7 +33,15 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-next()
+  const { scheme_name } = req.body
+  if (scheme_name === undefined || 
+    typeof scheme_name !== 'string' || 
+    !scheme_name.trim()
+    ) {
+      next({status: 400, message: 'invalid scheme_name'})
+    } else {
+      next()
+    }
 }
 
 /*
@@ -46,7 +54,18 @@ next()
   }
 */
 const validateStep = (req, res, next) => {
-next()
+  const { instructions, step_number } = req.body
+
+  if (instructions === undefined ||
+    typeof instructions !== 'string' ||
+    !instructions.trim() ||
+    typeof step_number !== 'number' ||
+    step_number < 1) {
+      const error = { status: 400, message: 'invalid step'}
+      next(error)
+    } else {
+      next()
+    }
 }
 
 module.exports = {
